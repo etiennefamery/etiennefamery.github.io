c9dfbbf5d57d094b2672272c17a8c882aada6e63
Etienne FAMERY <famery@d17-l427-pc-15.ipa.iisg.agh.edu.pl>
2019-03-25 08:49:40 +0100

    3rd commit

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <html>
+<header><title>My friends book</title></header>
 <body>
-<b>Header :</b>
+<h1>Friend book</h1>
+<form>
+	<label><input type="text">Name:</label>
+	<button type="submit">Submit</button>	
+<form>
 <?php
     echo $_GET["name"];
 ?>
