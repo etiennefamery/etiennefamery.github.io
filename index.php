17b9b52b74b37e1f9223fab68447aafd7c6a50b6
Etienne FAMERY <famery@d17-l427-pc-15.ipa.iisg.agh.edu.pl>
2019-03-25 09:33:16 +0100

    4 commit

--- index.php
+++ index.php
@@ -2,12 +2,28 @@
 <header><title>My friends book</title></header>
 <body>
 <h1>Friend book</h1>
-<form>
-	<label><input type="text">Name:</label>
-	<button type="submit">Submit</button>	
-<form>
+<form action="index.php" method="post">
+	<label>Name:<input type="text" name="name"></label>
+	<input type="submit">	
+</form>
 <?php
     echo $_GET["name"];
+    
+    
+    $filename = 'friends.txt';
+	if ($file = fopen( $filename, "r" )){
+		while (!feof($file)) {
+			$line = fgets($file);
+			echo $line;
+		}
+		fclose($file);
+	}
+
+	// appending to file
+	if (isset["name"] && (strlen($_POST["name"]))>0){
+	$file = fopen( $filename, "a" );
+	fwrite( $file, "some string" );
+	}
 ?>
 </body>
 </html>
