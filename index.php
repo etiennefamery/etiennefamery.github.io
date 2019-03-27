bc6a83f0a3297fd70ad914a1e5126ee5c1b6349e
Etienne FAMERY <famery@d17-l427-pc-15.ipa.iisg.agh.edu.pl>
2019-03-27 16:07:25 +0100

    commit with first version of friend book

--- index.php
+++ index.php
@@ -4,26 +4,40 @@
 <h1>Friend book</h1>
 <form action="index.php" method="post">
 	<label>Name:<input type="text" name="name"></label>
-	<input type="submit">	
-</form>
-<?php
-    echo $_GET["name"];
-    
-    
-    $filename = 'friends.txt';
-	if ($file = fopen( $filename, "r" )){
+	<input type="submit" value="Add new friend">	
+	<?php
+	// appending to file
+	if (isset($_POST["name"]) && strlen($_POST["name"])>0){
+		$filename = 'friends.txt';
+		$var = "<li>";
+		$var .= $_POST["name"];
+		$var .= "</li>";
+		$file = fopen( $filename, "a" );
+		fwrite( $file, $var );
+		fclose($file);
+		
+		if ($file = fopen( $filename, "r" )){
 		while (!feof($file)) {
 			$line = fgets($file);
 			echo $line;
 		}
 		fclose($file);
+		}
 	}
+	else{
+		$filename = 'friends.txt';
+		if ($file = fopen( $filename, "r" )){
+		while (!feof($file)) {
+			$line = fgets($file);
+			echo $line;
+		}
+		fclose($file);
+		}
+	}
+	?>
+	<input type="text" name="nameFilter" value="<?=$nameFilter?>">
+	<input type="submit" value="Filter list">	
+</form>
 
-	// appending to file
-	if (isset["name"] && (strlen($_POST["name"]))>0){
-	$file = fopen( $filename, "a" );
-	fwrite( $file, "some string" );
-	}
-?>
 </body>
 </html>
