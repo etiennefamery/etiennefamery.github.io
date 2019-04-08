bc3ee56fedbdee9c6d83e8364306d750500a35f0
etiennefamery <47968309+etiennefamery@users.noreply.github.com>
2019-04-08 10:54:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,39 +4,78 @@
 <h1>Friend book</h1>
 <form action="index.php" method="post">
 	<label>Name:<input type="text" name="name"></label>
-	<input type="submit" value="Add new friend">	
+	<input type="submit" value="Add new friend">
+	<input type="text" name="nameFilter" >
+	<input type="checkbox" name="checkbox" value="value1">Only names starting with</input>
+	<input type="submit" value="Filter list">	
 	<?php
+	
+	$filename= 'friends.txt';
 	// appending to file
 	if (isset($_POST["name"]) && strlen($_POST["name"])>0){
-		$filename = 'friends.txt';
-		$var = "<li>";
-		$var .= $_POST["name"];
-		$var .= "</li>";
-		$file = fopen( $filename, "a" );
+		$var = $_POST["name"];
+		$var .= "\n";	
+		$file = fopen($filename,"a");
 		fwrite( $file, $var );
 		fclose($file);
+		affichageEntier();
 		
+	// loop to find friends names which start with the value typed into "nameFilter"	
+	} elseif(isset($_POST["nameFilter"]) && strlen($_POST["nameFilter"])>0 && isset($_POST["checkbox"])){
+		$findme = $_POST["nameFilter"];
 		if ($file = fopen( $filename, "r" )){
-		while (!feof($file)) {
-			$line = fgets($file);
-			echo $line;
-		}
+			while (!feof($file)) {
+				$line = fgets($file);
+				$pos = strpos($line, $findme);
+				if ($pos !== false && $pos === 0){
+					$var = "<li>";
+					$var .= $line;
+					$var .= "</li>";
+					echo $var;
+				}
+			}
+		fclose($file);
+		}	
+	//loop to find all names which contains the value typed into "nameFilter"	
+	} elseif(isset($_POST["nameFilter"]) && strlen($_POST["nameFilter"])>0){
+		$findme = $_POST["nameFilter"];
+		if ($file = fopen( $filename, "r" )){
+			while (!feof($file)) {
+				$line = fgets($file);
+				$pos = strpos($line, $findme);
+				if ($pos !== false){
+					$var = "<li>";
+					$var .= $line;
+					$var .= "</li>";
+					echo $var;
+				}
+			}
 		fclose($file);
 		}
+		
+	} else{
+		affichageEntier();
 	}
-	else{
-		$filename = 'friends.txt';
-		if ($file = fopen( $filename, "r" )){
-		while (!feof($file)) {
-			$line = fgets($file);
-			echo $line;
+	
+	
+	
+		function affichageEntier(){
+			$filename= 'friends.txt';
+			if ($file = fopen( $filename, "r" )){
+				while (!feof($file)) {
+					$line = fgets($file);
+					$var = "<li>";
+					$var .= $line;
+					$var .= "</li>";
+					echo $var;
+				}
+				fclose($file);
+			}
 		}
-		fclose($file);
-		}
-	}
+		
+		
 	?>
-	<input type="text" name="nameFilter" value="<?=$nameFilter?>">
-	<input type="submit" value="Filter list">	
+	
 </form>
 
 </body>
